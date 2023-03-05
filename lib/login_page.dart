5955fc61d47123af41b9fc028c96ed7f18e4ff68
Javi2323J <jromera232003@gmail.com>
2023-03-05 20:49:22 +0100

    Login page FIX

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,16 +7,19 @@ import 'package:inoffensive/TransicionPage.dart';
 import 'package:inoffensive/components/square_tile.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+ LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[800],
       body: SafeArea(
         child: Center(
@@ -29,67 +32,67 @@ class _LoginPageState extends State<LoginPage> {
                       width: 400,
                       height: 200,
                       child: Image.asset('images/logo.png')),
-                ),
               ),
+            ),
 
-              const SizedBox(height: 20),
+            const SizedBox(height: 20),
 
-              //Entry 1
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'USERNAME',
-                          hintStyle:
-                              TextStyle(color: Colors.white.withOpacity(0.6))),
-                    ),
-                  ),
-                ),
-              ),
+            //Entry 1
+            Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.transparent,
+      border: Border.all(color: Colors.white),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    child: Padding(
+      padding: EdgeInsets.only(left: 20.0),
+      child: TextField(
+        controller: _usernameController, // Agregado
+        style: TextStyle(color: Colors.white, fontSize: 20),
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'USERNAME',
+          hintStyle: TextStyle(color: Colors.white.withOpacity(0.6))),
+        ),
+      ),
+    ),
+  ),
 
-              const SizedBox(height: 22),
+            const SizedBox(height: 22),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      obscureText: true,
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'PASSWORD',
-                        hintStyle:
-                            TextStyle(color: Colors.white.withOpacity(0.6)),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+            Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.transparent,
+      border: Border.all(color: Colors.white),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    child: Padding(
+      padding: EdgeInsets.only(left: 20.0),
+      child: TextField(
+        controller: _passwordController, // Agregado
+        obscureText: true,
+        style: TextStyle(color: Colors.white, fontSize: 20),
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'PASSWORD',
+          hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+        ),
+      ),
+    ),
+  ),
+),
 
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
                 child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  InkWell(
+                  InkWell( 
                     child: const Text(
-                      'Forgot password?',
+                      'I forgor',
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                     onTap: () {
@@ -101,43 +104,61 @@ class _LoginPageState extends State<LoginPage> {
                     },
                   ),
                 ]),
-              ),
-              const SizedBox(height: 40),
+            ),
+            const SizedBox(height: 40),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: SizedBox(
-                    height: 60,
-                    width: 600,
-                    child: ElevatedButton(
-                      child: Text(
-                        "LOGIN",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        elevation: 10,
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => TransicionPage()),
-                        );
-                      },
-                    )),
-              ),
+            Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+  child: SizedBox(
+    height: 60,
+    width: 600,
+    child: ElevatedButton(
+      child: Text(
+        "LOGIN",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        elevation: 10,
+      ),
+      onPressed: () {
+        if (_usernameController.text.isEmpty || _passwordController.text.isEmpty) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Please enter a username and password.'),
+                actions: [
+                  TextButton(
+                    child: Text('OK',style: TextStyle(fontWeight: FontWeight.bold),),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TransicionPage()),
+          );
+        }
+      },
+    ),
+  ),
+),
 
               const SizedBox(height: 50),
 
               // or continue with
               const SizedBox(height: 50),
-
-              Padding(
+              
+            Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
                   children: [
@@ -194,11 +215,11 @@ class _LoginPageState extends State<LoginPage> {
                     style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                   const SizedBox(width: 4),
-                  InkWell(
+                  InkWell( 
                     child: const Text(
                       'Register now',
                       style: TextStyle(
-                          color: Colors.blue,
+                        color: Colors.blue, 
                           fontWeight: FontWeight.bold,
                           fontSize: 16),
                     ),
@@ -213,8 +234,8 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               )
             ],
-          ),
-        ),
+              ),
+            ),
       ),
     );
   }
